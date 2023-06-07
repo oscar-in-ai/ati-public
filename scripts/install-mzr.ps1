396607d64ed98f90e0fa64c6578d653f18d3fd0e
oscar-lopv <oscar.lopezv@hotmail.com>
2023-06-07 20:20:32 +1000

    Added connection string and restore folder

--- scripts/install-mzr.ps1
+++ scripts/install-mzr.ps1
@@ -4,11 +4,20 @@ $FolderPath = Join-Path -Path $UserPath -ChildPath 'Freelit/mzr'
 $ConfigPath = Join-Path -Path $FolderPath -ChildPath 'config'
 $LogsPath = Join-Path -Path $FolderPath -ChildPath 'logs'
 $DistributionsPath = Join-Path -Path $FolderPath -ChildPath 'distributions'
+$RestorePath = Join-Path -Path $FolderPath -ChildPath 'restore'
 
 # Create folders
 New-Item -ItemType Directory -Path $ConfigPath -Force
 New-Item -ItemType Directory -Path $LogsPath -Force
 New-Item -ItemType Directory -Path $DistributionsPath -Force
+New-Item -ItemType Directory -Path $RestorePath -Force
+
+$TerminalNumber = Read-Host -Prompt 'Enter Terminal number'
+$ConfigFilePath = Join-Path -Path $ConfigPath -ChildPath ('{0}.txt' -f $TerminalNumber)
+$ConfigContent = 'user id=;password=;port=3306;server=;database=;persistsecurityinfo=True;convertzerodatetime=True'
+
+# Create the config file
+$ConfigContent | Out-File -FilePath $ConfigFilePath -Encoding UTF8
 
 $ZipFileUrl = 'https://ati-s3-public-dist-prd.s3.ap-southeast-2.amazonaws.com/distributions/mzr/mzr_latest.zip'
 $TempPath = [System.IO.Path]::GetTempPath()
