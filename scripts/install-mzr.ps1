57f1aa2264c5bcb098752741847e2760788fccef
oscar-lopv <oscar.lopezv@hotmail.com>
2023-06-07 20:12:57 +1000

    Update install-mzr.ps1

--- scripts/install-mzr.ps1
+++ scripts/install-mzr.ps1
@@ -1,24 +1,25 @@
-# Set the paths
-$basePath = [Environment]::GetFolderPath("UserProfile")
-$freelitPath = Join-Path $basePath "Documents\Freelit"
-$mzrPath = Join-Path $freelitPath "mzr"
-$configPath = Join-Path $mzrPath "config"
-$logsPath = Join-Path $mzrPath "logs"
-$distributionsPath = Join-Path $mzrPath "distributions"
-$tempPath = Join-Path $basePath "Documents\Temp"
+$UserPath = [Environment]::GetFolderPath('MyDocuments')
+$FolderPath = Join-Path -Path $UserPath -ChildPath 'Freelit/mzr'
 
-# Create the necessary folders
-New-Item -ItemType Directory -Force -Path $configPath
-New-Item -ItemType Directory -Force -Path $logsPath
-New-Item -ItemType Directory -Force -Path $distributionsPath
+$ConfigPath = Join-Path -Path $FolderPath -ChildPath 'config'
+$LogsPath = Join-Path -Path $FolderPath -ChildPath 'logs'
+$DistributionsPath = Join-Path -Path $FolderPath -ChildPath 'distributions'
+
+# Create folders
+New-Item -ItemType Directory -Path $ConfigPath -Force
+New-Item -ItemType Directory -Path $LogsPath -Force
+New-Item -ItemType Directory -Path $DistributionsPath -Force
+
+$ZipFileUrl = 'https://ati-s3-public-dist-prd.s3.ap-southeast-2.amazonaws.com/distributions/mzr/mzr_latest.zip'
+$TempPath = [System.IO.Path]::GetTempPath()
+$ZipFilePath = Join-Path -Path $TempPath -ChildPath 'mzr_latest.zip'
 
 # Download the ZIP file
-$zipUrl = "https://ati-s3-public-dist-prd.s3.ap-southeast-2.amazonaws.com/distributions/mzr/mzr_latest.zip"
-$zipPath = Join-Path $tempPath "mzr_latest.zip"
-Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+Invoke-WebRequest -Uri $ZipFileUrl -OutFile $ZipFilePath
 
-# Extract the contents of the ZIP file
-Expand-Archive -Path $zipPath -DestinationPath $distributionsPath -Force
+# Extract contents to the distributions folder
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFilePath, $DistributionsPath)
 
 # Clean up the temporary ZIP file
-Remove-Item -Path $zipPath -Force
+Remove-Item -Path $ZipFilePath -Force
